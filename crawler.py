0e97d219e94190c2fdb1b0f860d540fbd22076a6
MichalRybecky <michalrybec@protonmail.com>
2021-01-14 15:27:42 +0100

    First version of crawler.py done. It is able to: - read ticker symbols from text file - create Yahoo Finance urls based on these ticker symbols - parse the current stock price from given urls - return list of dictionaries with ticker symbol and it's current price

--- crawler.py
+++ crawler.py
@@ -1,9 +1,35 @@
+"""
+Yahoo Finance current stock price crawler
+- takes ticker symbols from ticker-symbols.txt
+- returns list of dictionaries with ticker symbol and it's current price
+"""
+
 import requests
 from bs4 import BeautifulSoup
 
-page = "https://finance.yahoo.com/quote/CCIV?p=CCIV"
-response = requests.get(page)
-PAGE_HTML = response.text
-soup = BeautifulSoup(PAGE_HTML, 'html.parser')
-price = soup.find('span', attrs={"data-reactid": "32"}).text
-print(price)
+
+def get_urls() -> list:
+    """
+    returns list of dictionaries with ticker symbol and it's Yahoo Finance url
+    """
+    with open('ticker-symbols.txt', 'r') as file:
+        tickers = [line.strip() for line in file]
+    urls = []
+    for ticker in tickers:
+        urls.append({'ticker': ticker, 'url': f'https://finance.yahoo.com/quote/{ticker}?p={ticker}'})
+    return urls
+
+
+def get_prices() -> list:
+    """
+    returns list of dictionaries with ticker symbol and it's current price
+    """
+    prices = []
+    for url in get_urls():
+        response = requests.get(url['url'])
+        PAGE_HTML = response.text
+        soup = BeautifulSoup(PAGE_HTML, 'html.parser')
+        price = soup.find('span', attrs={"data-reactid": "32"}).text
+        prices.append({'ticker': url['ticker'], 'price': float(price)})
+    return prices
+
