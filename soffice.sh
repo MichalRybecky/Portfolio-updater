7f946b67f43d9e67010faf16554c53075f3dc3c5
MichalRybecky <michalrybec@protonmail.com>
2021-01-14 14:34:04 +0100

    initial file structure, copied soffice.sh script for running LibreOffice Calc based on OS

---
+++ soffice.sh
@@ -0,0 +1,18 @@
+
+# Linux:
+# soffice --calc --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager"
+# Mac:
+# /Applications/LibreOffice.app/Contents/MacOS/soffice --calc \
+# --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager"
+# Windows:
+# "C:\\Program Files (x86)\LibreOffice 5\program\soffice.exe" --calc --accept="socket,host=localhost,port=2002;urp;"
+
+# Checking for OS type and running LibreOffice Calc
+case "$OSTYPE" in
+  solaris*) echo "SOLARIS OS is not supported" ;;
+  darwin*)  /Applications/LibreOffice.app/Contents/MacOS/soffice --calc --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager" ;;
+  linux*)   soffice --calc --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager" ;;
+  bsd*)     echo "BSD OS is not supported" ;;
+  msys*)    "C:\\Program Files (x86)\LibreOffice 5\program\soffice.exe" --calc --accept="socket,host=localhost,port=2002;urp;" ;;
+  *)        echo "unknown: $OSTYPE" ;;
+esac
