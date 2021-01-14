0a88a7d3c0c2b70de35f5c7f8aa058aef9a26464
MichalRybecky <michalrybec@protonmail.com>
2021-01-14 17:28:19 +0100

    problems with soffice's OS detection, soffice.sh will run only command for linux

--- soffice.sh
+++ soffice.sh
@@ -8,11 +8,13 @@
 # "C:\\Program Files (x86)\LibreOffice 5\program\soffice.exe" --calc --accept="socket,host=localhost,port=2002;urp;"
 
 # Checking for OS type and running LibreOffice Calc
-case "$OSTYPE" in
-  solaris*) echo "SOLARIS OS is not supported" ;;
-  darwin*)  /Applications/LibreOffice.app/Contents/MacOS/soffice --calc --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager" ;;
-  linux*)   soffice --calc --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager" ;;
-  bsd*)     echo "BSD OS is not supported" ;;
-  msys*)    "C:\\Program Files (x86)\LibreOffice 5\program\soffice.exe" --calc --accept="socket,host=localhost,port=2002;urp;" ;;
-  *)        echo "unknown: $OSTYPE" ;;
-esac
+# case "$OSTYPE" in
+#   solaris*) echo "SOLARIS OS is not supported" ;;
+#   darwin*)  /Applications/LibreOffice.app/Contents/MacOS/soffice --calc --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager" ;;
+#   linux*)   soffice --calc --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager" ;;
+#   bsd*)     echo "BSD OS is not supported" ;;
+#   msys*)    "C:\\Program Files (x86)\LibreOffice 5\program\soffice.exe" --calc --accept="socket,host=localhost,port=2002;urp;" ;;
+#   *)        echo "unknown: $OSTYPE" ;;
+# esac
+
+soffice --accept="socket,host=localhost,port=2002;urp;" --norestore --nologo --nodefault # --headless
