31a8709ba568dd99e1b7579e505f1fdc104b62f9
MichalRybecky <michalrybec@protonmail.com>
2021-01-14 18:11:14 +0100

    reformatting using black

--- crawler.py
+++ crawler.py
@@ -12,11 +12,16 @@ def get_urls() -> list:
     """
     returns list of dictionaries with ticker symbol and it's Yahoo Finance url
     """
-    with open('ticker-symbols.txt', 'r') as file:
+    with open("ticker-symbols.txt", "r") as file:
         tickers = [line.strip().upper() for line in file]
     urls = []
     for ticker in tickers:
-        urls.append({'ticker': ticker, 'url': f'https://finance.yahoo.com/quote/{ticker}?p={ticker}'})
+        urls.append(
+            {
+                "ticker": ticker,
+                "url": f"https://finance.yahoo.com/quote/{ticker}?p={ticker}",
+            }
+        )
     return urls
 
 
@@ -26,13 +31,12 @@ def get_prices() -> list:
     """
     prices = []
     for url in get_urls():
-        response = requests.get(url['url'])
+        response = requests.get(url["url"])
         PAGE_HTML = response.text
-        soup = BeautifulSoup(PAGE_HTML, 'html.parser')
+        soup = BeautifulSoup(PAGE_HTML, "html.parser")
         try:
-            price = soup.find('span', attrs={"data-reactid": "32"}).text
+            price = soup.find("span", attrs={"data-reactid": "32"}).text
         except AttributeError:
-            price = 'Unable to parse, check ticker!'
-        prices.append({'ticker': url['ticker'], 'price': price})
+            price = "Unable to parse, check ticker!"
+        prices.append({"ticker": url["ticker"], "price": price})
     return prices
-
