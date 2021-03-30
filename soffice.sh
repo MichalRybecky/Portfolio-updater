dd2d53c567d391b2af73471ca9db69a08185bfd8
Michal Rybecky <michalrybec@protonmail.com>
2021-03-30 22:58:54 +0200

    Fixed the issue where the script opened two spreadsheets, one empty and the second correct one

--- soffice.sh
+++ soffice.sh
@@ -10,9 +10,8 @@
 # Checking for OS type and running LibreOffice Calc
 case "$OSTYPE" in
   darwin*)  /Applications/LibreOffice.app/Contents/MacOS/soffice --calc --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager" ;;
-  linux*)   soffice --calc --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager" ;;
+  linux*)   soffice --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager" ;;
   msys*)    "C:\\Program Files (x86)\LibreOffice 5\program\soffice.exe" --calc --accept="socket,host=localhost,port=2002;urp;" ;;
   *)        echo "unknown: $OSTYPE" ;;
 esac
 
-#soffice --accept="socket,host=localhost,port=2002;urp;" --norestore --nologo --nodefault # --headless
