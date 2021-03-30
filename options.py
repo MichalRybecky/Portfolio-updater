bef515403de1ad8605c31cec35d7aaf1820ace70
Michal Rybecky <michalrybec@protonmail.com>
2021-03-30 22:17:48 +0200

    Added filepath to options.py

--- options.py
+++ options.py
@@ -2,9 +2,13 @@
 This module is for customizing different aspects of the app
 """
 
+# Full path of the Calc file you want to work with
+# path = string
+path = ""
+
 # Number of sheet that we want to work with
 # sheet_number = int
-sheet_number = 5
+sheet_number = 1
 
 # Position we want to start looking for ticker symbols
 # start_positon = [row, column]
