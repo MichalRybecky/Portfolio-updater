eb0c21f8d9fb27983e56c44cc164a9d090ba99cd
Michal Rybecky <michalrybec@protonmail.com>
2021-03-31 23:06:27 +0200

    Added parameters when launching soffice to disable startup logo and prevent two instances of soffice running at the same time

--- soffice.sh
+++ soffice.sh
@@ -10,7 +10,7 @@
 # Checking for OS type and running LibreOffice Calc
 case "$OSTYPE" in
   darwin*)  /Applications/LibreOffice.app/Contents/MacOS/soffice --calc --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager" ;;
-  linux*)   soffice --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager" ;;
+  linux*)   soffice --accept="socket,host=localhost,port=2002;urp;StarOffice.ServiceManager" --norestore --nologo --nodefault;;
   msys*)    "C:\\Program Files (x86)\LibreOffice 5\program\soffice.exe" --calc --accept="socket,host=localhost,port=2002;urp;" ;;
   *)        echo "unknown: $OSTYPE" ;;
 esac
